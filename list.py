dd388304ec2bfeb58d3ba70f432c08cb33b407bd
Rober Torres <robertdrakenfor@gmail.com>
2024-02-13 21:24:33 -0500

    Listas

--- list.py
+++ list.py
@@ -4,6 +4,14 @@ numeros = [1, 2, 3, 4, 5]
 # Puedo verlos en consola
 print("La lista de numeros: ", numeros)
 
-# Crando una segunda lista
+# Creando una segunda lista
 lista2 = [4, 3, 2, 4, 5]
 print("Lista 2: ", lista2)
+
+# Imprimiendo las 2 listas
+print("Numeros: ", numeros, ", Lista 2: ", lista2)
+
+
+# Listas de tipo cadenas de texto (strings)
+nombres = ["Juan", "Pedro", "Maria"]
+print("Nombres: ", nombres)
