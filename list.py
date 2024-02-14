18ced9d5c7dec4805881f3cda39b9c3a8a79dbf0
Rober Torres <robertdrakenfor@gmail.com>
2024-02-13 20:34:36 -0500

    first commit

---
+++ list.py
@@ -0,0 +1,9 @@
+# Listas
+numeros = [1, 2, 3, 4, 5]
+
+# Puedo verlos en consola
+print("La lista de numeros: ", numeros)
+
+# Crando una segunda lista
+lista2 = [4, 3, 2, 4, 5]
+print("Lista 2: ", lista2)
