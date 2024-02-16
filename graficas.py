b150b4420fb71de26cdaca5b0af2c1d025d46bed
Rober Torres <robertdrakenfor@gmail.com>
2024-02-15 21:00:03 -0500

    Graficas primera parte

---
+++ graficas.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [1, 2, 3, 4, 5]
+
+# Gráficar una nuve de puntos
+plt.scatter(x, y)
+
+# Agregar titúlo
+plt.title("Nuve de puntos")
+
+# Mostrar el gráfico
+plt.show()
